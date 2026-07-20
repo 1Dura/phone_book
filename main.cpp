b1919b0c8804b12e632b9c18e755718e2897675a
1Dura <BeSTaR_X@mail.ru>
2026-07-20 15:41:33 +0300

    started console interface for phonebook (add show exit commands)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
 #include "personClass.hpp"
 #include <iostream>
+#include <list>
+#include <string>
 
 int main() {
-  person p1;
-  person p2("John", "Clark", 79157617240);
-  std::cout << p2.getFirstName();
+  const std::string welcomeText = "Welcome to phone book! \nUse can use "
+                                  "commands:\n/show\n/add\n/remove\n/exit\n";
+  std::list<person> book;
+  while (true) {
+    std::cout << welcomeText;
+    std::string command;
+    std::cin >> command;
+    if (command == "/show") {
+      for (auto p : book) {
+        p.print();
+      }
+    } else if (command == "/add") {
+      std::string fName;
+      std::string sName;
+      long long pNumber = -1;
+      std::cout << "Enter first name: ";
+      std::cin >> fName;
+      std::cout << "Enter second name: ";
+      std::cin >> sName;
+      std::cout << "Enter phone number: ";
+      std::cin >> pNumber;
+      book.push_back(person(fName, sName, pNumber));
+    } else if (command == "/exit") {
+      break;
+    }
+    // else if (command == "/remove") {
+    //   size_t id;
+    //   std::cout << "Enter id you want remove: ";
+    //   std::cin >> id;
+    // }
+  }
 }
